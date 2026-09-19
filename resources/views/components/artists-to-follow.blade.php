c259d44cc0a447dcda49aaa6a9e3a0450d66a918
DMelnikk <dmitry.melnik001@mail.ru>
2026-09-19 18:13:47 +0300

    ....

---
+++ resources/views/components/artists-to-follow.blade.php
@@ -0,0 +1,18 @@
+<div class="mt-10 border border-pixl-light/10 p-4">
+    <h2 class="text-sm text-pixl-light/60">Artists to follow</h2>
+    <ol class="flex flex-col gap-4 mt-4">
+        @foreach($artists as $artist)
+            <li class="flex items-center justify-between gap-4">
+                <div class="flex gap-2.5 items-center">
+                    <img src="/images/{{$artist['img']}}" alt="Avatar of {{$artist['name']}}" class="size-8 object-cover">
+                    <p class="text-sm truncate">{{$artist['name']}}</p>
+
+                </div>
+                <button class="bg-pixl-dark/50 hover:bg-pixl-dark/60 border active:bg-pixl-dark/75 border-pixl/50 hover:border-pixl/60 active:border-pixl/75  px-2 py-1 text-pixl text-sm">
+                    Follow
+                </button>
+            </li>
+        @endforeach
+    </ol>
+    <a href="#" class="text-pixl-light/60 text-sm inline-block mt-4">Show more</a>
+</div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="color-scheme" content="dark">
     @vite(['resources/css/app.css','resources/js/app.js'])
-    <title>PIXL</title>
+    <title>{{$title}}</title>
 </head>
 <body class="bg-pixl-dark text-pixl-light flex gap-8 xl:gap-16 px-4 sm:h-dvh sm:overflow-clip">
     {{$slot}}
